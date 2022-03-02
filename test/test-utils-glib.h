6a4a89ccea6035c61998f58080ce53f7151853fa
Simon McVittie <smcv@collabora.com>
2022-03-02 13:04:11 +0000

    test-utils-glib: Add backports of some convenience macros
    Signed-off-by: Simon McVittie <smcv@collabora.com>

--- test/test-utils-glib.h
+++ test/test-utils-glib.h
@@ -126,6 +126,18 @@ backported_g_steal_pointer (gpointer pointer_to_pointer)
 }
 #endif
 
+#ifndef g_assert_true
+#define g_assert_true(a) g_assert (a)
+#endif
+
+#ifndef g_assert_false
+#define g_assert_false(a) g_assert (!(a))
+#endif
+
+#ifndef g_assert_null
+#define g_assert_null(a) g_assert ((a) == NULL)
+#endif
+
 #ifndef g_assert_nonnull
 #define g_assert_nonnull(a) g_assert ((a) != NULL)
 #endif
