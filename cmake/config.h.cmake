f0a4d9249078b5ce7f37aea254a4911e481ef584
Marc-André Lureau <marcandre.lureau@redhat.com>
2022-07-08 19:38:18 +0400

    cmake: drop needless headers checks
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- cmake/config.h.cmake
+++ cmake/config.h.cmake
@@ -101,16 +101,8 @@
 /* Define to 1 if you have dirent.h */
 #cmakedefine   HAVE_DIRENT_H 1
 
-#cmakedefine HAVE_DLFCN_H 1
-
 /* Define to 1 if you have errno.h */
 #cmakedefine   HAVE_ERRNO_H 1
-
-#cmakedefine HAVE_EXECINFO_H 1
-#cmakedefine HAVE_EXPAT_H 1
-
-/* Define to 1 if you have grp.h */
-#cmakedefine   HAVE_GRP_H 1
 
 /* Define to 1 if you have inttypes.h */
 #cmakedefine   HAVE_INTTYPES_H 1
@@ -121,8 +113,6 @@
 /* Define to 1 if you have locale.h */
 #cmakedefine   HAVE_LOCALE_H 1
 
-#cmakedefine HAVE_MEMORY_H 1
-
 /* Define to 1 if you have poll */
 #cmakedefine    HAVE_POLL 1
 
@@ -132,36 +122,21 @@
 /* Define to 1 if you have stdint.h */
 #cmakedefine   HAVE_STDINT_H 1
 
-#cmakedefine HAVE_STDLIB_H 1
-
 /* Define to 1 if you have stdio.h */
 #cmakedefine   HAVE_STDIO_H 1
 
-#cmakedefine HAVE_STRINGS_H 1
-#cmakedefine HAVE_STRING_H 1
 #cmakedefine HAVE_SYSLOG_H 1
 #cmakedefine HAVE_SYS_EVENTS_H 1
 #cmakedefine HAVE_SYS_INOTIFY_H 1
 #cmakedefine HAVE_SYS_PRCTL_H 1
 #cmakedefine HAVE_SYS_RANDOM_H 1
 #cmakedefine HAVE_SYS_RESOURCE_H 1
-#cmakedefine HAVE_SYS_STAT_H 1
 
 /* Define to 1 if you have sys/syslimits.h */
 #cmakedefine   HAVE_SYS_SYSLIMITS_H 1
 
 /* Define to 1 if you have sys/time.h */
 #cmakedefine    HAVE_SYS_TIME_H 1
-
-#cmakedefine HAVE_SYS_TYPES_H 1
-
-#cmakedefine HAVE_SYS_UIO_H 1
-
-/* Define to 1 if you have sys/wait.h */
-#cmakedefine    HAVE_SYS_WAIT_H 1
-
-/* Define to 1 if you have time.h */
-#cmakedefine   HAVE_TIME_H 1
 
 /* Define to 1 if you have unistd.h */
 #cmakedefine   HAVE_UNISTD_H 1
