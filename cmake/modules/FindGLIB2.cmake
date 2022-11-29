WangTingMan/dbus_win

commit d6abc1da02959728236457e4a6b5b1b9a61b3427

cmake: Add BSD-style licenses
I've erred on the side of caution and treated the COPYING-CMAKE-SCRIPTS
license (a BSD-3-Clause variation) as its own distinct license.

Co-authored-by: Ralf Habacker <ralf.habacker@freenet.de>
Signed-off-by: Simon McVittie <smcv@collabora.com>

cmake/modules/FindGLIB2.cmake
@@ -7,9 +7,10 @@
 
 # Copyright (c) 2008 Laurent Montel, <montel@kde.org>
 # Copyright (c) 2013 Ralf Habacker, <ralf.habacker@freenet.de>
+# SPDX-License-Identifier: LicenseRef-CMakeScripts.txt
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
-# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+# For details see LICENSES/LicenseRef-CMakeScripts.txt.
 
 
 if(GLIB2_INCLUDE_DIR AND GLIB2_LIBRARIES)

Makefile.am
@@ -15,7 +15,9 @@ EXTRA_DIST =			\
 	LICENSES/AFL-2.0.txt	\
 	LICENSES/AFL-2.1.txt	\
 	LICENSES/Apache-2.0.txt	\
+	LICENSES/BSD-3-Clause.txt	\
 	LICENSES/GPL-2.0-or-later.txt	\
+	LICENSES/LicenseRef-CMakeScripts.txt	\
 	LICENSES/LicenseRef-GAP.txt	\
 	LICENSES/LicenseRef-pycrypto-orig.txt	\
 	LICENSES/MIT.txt	\

cmake/modules/FindDocBookXSL.cmake
@@ -6,6 +6,7 @@
 #  used to process DocBook XML
 
 # Copyright (c) 2010, Luigi Toscano, <luigi.toscano@tiscali.it>
+# SPDX-License-Identifier: BSD-3-Clause
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions

cmake/modules/Win32Macros.cmake
@@ -2,6 +2,7 @@
 # win32 macros 
 # 
 # Copyright (c) 2006-2007, Ralf Habacker
+# SPDX-License-Identifier: BSD-3-Clause
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 #

commit 0827d765534efd0a4407b51993a7f02bae94d172

Fix SPDX license reference in FindGLIB2
The `.txt` here was unnecessary.

Fixes: d6abc1da "cmake: Add BSD-style licenses"
Signed-off-by: Simon McVittie <smcv@collabora.com>

cmake/modules/FindGLIB2.cmake
@@ -7,7 +7,7 @@
 
 # Copyright (c) 2008 Laurent Montel, <montel@kde.org>
 # Copyright (c) 2013 Ralf Habacker, <ralf.habacker@freenet.de>
-# SPDX-License-Identifier: LicenseRef-CMakeScripts.txt
+# SPDX-License-Identifier: LicenseRef-CMakeScripts
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see LICENSES/LicenseRef-CMakeScripts.txt.